8301b3bf7505c57b161cfe15d5014bd677364ddc
Richard Pilbery <tricky999@Richards-iMac.local>
2022-04-25 22:44:19 +0100

    basic button

--- app.py
+++ app.py
@@ -8,28 +8,24 @@ app = Dash(__name__)
 server = app.server
 
 app.layout = html.Div([
-    html.Div(
-    children=[
-        html.H1(children="111 Discrete Event Simulation model"),
-        html.P(
-            children="Click on the button below to run the simulation",
-        ),
-    ]),
+    html.Div(dcc.Input(id='input-on-submit', type='text')),
     html.Button('Submit', id='submit-val', n_clicks=0),
-    html.Div(id='container-button-basic', children='Enter a value and press submit')
+    html.Div(id='container-button-basic',
+             children='Enter a value and press submit')
 ])
+
 
 @app.callback(
     Output('container-button-basic', 'children'),
     Input('submit-val', 'n_clicks'),
     State('input-on-submit', 'value')
 )
-def input_on_submit(n_clicks, value):
-    parallelProcess(4)
+def update_output(n_clicks, value):
     return 'The input value was "{}" and the button has been clicked {} times'.format(
         value,
         n_clicks
     )
+
 
 
 
