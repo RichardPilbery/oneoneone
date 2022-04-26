2ec8aa166a7d88551e48309a76959b226487b97e
Richard Pilbery <tricky999@Richards-iMac.local>
2022-04-26 21:07:36 +0100

    Fixed logging messages

--- callbacks.py
+++ callbacks.py
@@ -1,7 +1,10 @@
 from dash import html, Input, Output, DashLogger
 from app import app
+import logging
 
 buttonClickCount = 0
+
+logging.basicConfig(format='%(asctime)s %(message)s', filename='simconfig.log', encoding='utf-8', level=logging.DEBUG)
 
 # Sim configuration callback
 @app.callback(
@@ -17,7 +20,7 @@ def configSim(sim_duration, warm_up_time, number_of_runs, run_sim, logger: DashL
     if(run_sim > buttonClickCount):
         # Run the sim
         output = f"Sim duration: {sim_duration}<br/>Warm-up time: {warm_up_time}</br>Number of runs: {number_of_runs}"
-        logger.info("Data submitted")
-        logger.info(output)
+        logging.debug("Data submitted")
+        logging.debug(output)
         buttonClickCount = run_sim
         return html.P(output)
