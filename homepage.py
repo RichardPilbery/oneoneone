0997bd1a9deb586a45cf5fe71f3f42a7fe192f6f
Richard Pilbery <tricky999@Richards-iMac.local>
2022-04-26 20:35:51 +0100

    Still trying

--- homepage.py
+++ homepage.py
@@ -1,4 +1,4 @@
-from dash import html, dcc
+from dash import dcc, html, Input, Output
 import dash_bootstrap_components as dbc
 
 
@@ -76,3 +76,17 @@ home = html.Div([
     ]),
 ])
 
+@app.callback(
+    Output('config', 'children'),
+    [Input('sim_duration', 'value'),
+    Input('warm_up_time', 'value'),
+    Input('number_of_runs', 'value'),
+    Input('run_sim', 'n_clicks')]
+)
+def configSim(sim_duration, warm_up_time, number_of_runs, run_sim):
+    global buttonClickCount
+
+    if(run_sim > buttonClickCount):
+        # Run the sim
+        buttonClickCount == run_sim
+        return html.P(f"Sim duration: {sim_duration}<br/>Warm-up time: {warm_up_time}</br>Number of runs: {number_of_runs}")
