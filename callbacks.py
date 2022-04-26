f44c3349298c2f21d9e058984f0471205f3a8ad5
Richard Pilbery <tricky999@Richards-iMac.local>
2022-04-26 21:04:26 +0100

    Added logging

--- callbacks.py
+++ callbacks.py
@@ -1,20 +1,23 @@
-from dash import html, Input, Output
+from dash import html, Input, Output, DashLogger
 from app import app
 
-buttonClick = 0
+buttonClickCount = 0
 
 # Sim configuration callback
 @app.callback(
     Output('config', 'children'),
-    [Input('sim_duration', 'value'),
+    Input('sim_duration', 'value'),
     Input('warm_up_time', 'value'),
     Input('number_of_runs', 'value'),
-    Input('run_sim', 'n_clicks')]
+    Input('run_sim', 'n_clicks')
 )
-def configSim(sim_duration, warm_up_time, number_of_runs, run_sim):
+def configSim(sim_duration, warm_up_time, number_of_runs, run_sim, logger: DashLogger):
     global buttonClickCount
 
     if(run_sim > buttonClickCount):
         # Run the sim
+        output = f"Sim duration: {sim_duration}<br/>Warm-up time: {warm_up_time}</br>Number of runs: {number_of_runs}"
+        logger.info("Data submitted")
+        logger.info(output)
         buttonClickCount = run_sim
-        return html.P(f"Sim duration: {sim_duration}<br/>Warm-up time: {warm_up_time}</br>Number of runs: {number_of_runs}")
+        return html.P(output)
