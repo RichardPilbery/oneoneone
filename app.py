c8f7bbb6d554bd7cb1dccd57d22af0ada3acd5f6
Richard Pilbery <tricky999@Richards-iMac.lan>
2022-04-27 15:09:03 +0100

    Using long callbacks

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from dash import Dash
 import dash_bootstrap_components as dbc
+from dash.long_callback import DiskcacheLongCallbackManager
 import logging
+import diskcache
+
+cache = diskcache.Cache("./cache")
+long_callback_manager = DiskcacheLongCallbackManager(cache)
 
 logging.basicConfig(format='%(asctime)s %(message)s', filename='model.log', encoding='utf-8', level=logging.DEBUG)
 
-app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP], long_callback_manager=long_callback_manager)
 server = app.server
--- callbacks.py
+++ callbacks.py
@@ -1,61 +1,26 @@
-from dash import html, Input, Output
+from dash import html, Input, Output, State
+from dash.long_callback import DiskcacheLongCallbackManager
 from app import app
 from layouts import SHOW_BUTTON_STYLE, HIDE_BUTTON_STYLE
 import logging
 from oneoneonedes import parallelProcess, prepStartingVars
 
-buttonClickCount = 0
-
-
-# 1. User clicks 'Run Simulation button'
-# Callback updates trigger_sim value
-@app.callback(
-    Output('toggle_button', 'value'),
-    Input('sim_running', 'value'),
-    Input('sim_complete', 'value'),
-)
-def runSimulation(sim_running, sim_complete):
-
-    logging.debug(f"Button clicked and sim_running is {sim_running} and sim_complete is {sim_complete}")
-
-    if sim_running == 0 and sim_complete == 0:
-        return 0
-    elif sim_running == 1 and sim_complete == 0:
-        return 1
-    elif sim_running == 1 and sim_complete == 1:
-        logging.debug('Togglebutton is set to zero')
-        return 0
-
-
-# 2. This will run and control button visibility when either 
-# trigger_sim or sim_complete values are changed
-@app.callback(
-    Output('submit_button', 'style'),
-    Output('sim_run_button', 'style'),
-    Output('sim_running', 'value'),
-    Input('run_sim', 'n_clicks'),
-    Input('toggle_button', 'value')
-)
-def clickButton(run_sim, toggle_button):
-    global buttonClickCount
-
-    if run_sim > buttonClickCount or toggle_button == 1:
-        logging.debug('Button click, time to hide')
-        buttonClickCount == run_sim
-        return HIDE_BUTTON_STYLE, SHOW_BUTTON_STYLE, 1
-    else:
-        logging.debug('Back to normal button behaviour')
-        return  SHOW_BUTTON_STYLE, HIDE_BUTTON_STYLE, 0
 
 
 # 3. When trigger sim is set to value = 1, then simulation will run
 # When it has finished, sim_complete is set to 1
-@app.callback(
-    Output('sim_complete', 'value'),
-    Input('submit_button', 'style'),
-    Input('sim_duration', 'value'),
-    Input('warm_up_time', 'value'),
-    Input('number_of_runs', 'value'),
+@app.long_callback(
+    outputs = Output('sim_complete', 'value'),
+    inputs = Input('run_sim', 'n_clicks'),
+    states = [
+        State('sim_duration', 'value'),
+        State('warm_up_time', 'value'),
+        State('number_of_runs', 'value'),
+    ],
+    running = [
+        (Output('submit_button', 'style'), HIDE_BUTTON_STYLE, SHOW_BUTTON_STYLE),
+        (Output('sim_run_button', 'style'), SHOW_BUTTON_STYLE, HIDE_BUTTON_STYLE),
+    ]
 )
 def configSim(submit_button_style, sim_duration, warm_up_time, number_of_runs):
     if submit_button_style == HIDE_BUTTON_STYLE:
